e86c2c9ade73ad1c65a99d5fa6a88f08241dc061
Sukrut <dekhanesd7@gmail.com>
2025-03-10 17:25:51 +0000

    remove comment

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,7 +9,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-#add register
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
